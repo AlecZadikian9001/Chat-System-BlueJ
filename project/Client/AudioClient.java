c89999666fb5c83c0ec18efea05678cbc180f60e
Alec Zadikian <alec@genmobapp.com>
2013-09-18 19:46:35 -0700

    more audio stuff

---
+++ project/Client/AudioClient.java
@@ -0,0 +1,87 @@
+package Client;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class AudioClient extends Thread //sending data to server socket
+{
+    private final static float MAX_8_BITS_SIGNED = Byte.MAX_VALUE;
+    private final static float MAX_8_BITS_UNSIGNED = 0xff;
+    private final static float MAX_16_BITS_SIGNED = Short.MAX_VALUE;
+    private final static float MAX_16_BITS_UNSIGNED = 0xffff;
+    private float level, sampleRate;
+    private TargetDataLine targetDataLine;
+    private AudioFormat format;
+    private int bufferSize;
+    private DataLine.Info sendInfo, playbackInfo;
+    
+    private Socket socket;
+    private OutputStream out;
+    private InputStream in;
+    private byte[] buffer;
+    private byte[]  playbackData;
+    private SourceDataLine  line;
+
+    private boolean isRunning;
+
+    public static void main(String[] args){
+        String address = args[0];
+        int port = Integer.parseInt(args[1]);
+        try{
+            new AudioClient(new Socket(address, port)); } catch (Exception e) {}
+    }
+
+    public AudioClient (Socket socket){ //put in socket with server address
+        this.socket = socket;
+        sampleRate = 44100;
+        int sampleSizeInBits = 16;
+        int channels = 1;
+        boolean signed = true;
+        boolean bigEndian = false;
+        format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+        sendInfo = new DataLine.Info(TargetDataLine.class, format);
+        playbackInfo = new DataLine.Info(SourceDataLine.class, format);
+        bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+        buffer = new byte[bufferSize];
+        Thread t = new Thread(this); t.start();
+    }
+
+    public void run(){
+        isRunning = true;
+        try{
+            targetDataLine = (TargetDataLine)AudioSystem.getLine(sendInfo);
+            targetDataLine.open(format, (int)sampleRate);
+            targetDataLine.start();
+
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+            line = (SourceDataLine) AudioSystem.getLine(playbackInfo);
+			line.open(format);
+			line.start();
+			int     nBytesRead = 0;
+			playbackData = new byte[128000];
+			
+            while (isRunning && nBytesRead!=-1) {
+                targetDataLine.read(buffer, 0, buffer.length);
+                out.write(buffer); //send sound
+                //receive and output sound:
+                nBytesRead = in.read(playbackData, 0, playbackData.length);
+                if (nBytesRead >=0){
+                     line.write(playbackData,  0, nBytesRead);
+                }
+            }
+            
+            line.drain();
+		line.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopRunning(){ try{isRunning = false; socket.close();} catch(Exception e){} }
+}
--- project/Server/AudioThread.java
+++ project/Server/AudioThread.java
@@ -3,32 +3,68 @@ import java.net.*;
 import java.util.*;
 import java.io.*;
 
-public class AudioThread{
+public class AudioThread extends Thread{
 
     /* This is the server socket to accept connections */
-        private ServerSocket serverSocket;
-    
+    private ServerSocket serverSocket;
+
     private boolean isRunning; //is the server up?
 
+    //I/O
+    private InputStream in1, in2;
+    private OutputStream out1, out2;
+    private Socket clientSocket1;
+    private Socket clientSocket2;
+    private byte[] client1Data;
+    private byte[] client2Data;
+
     public static void main (String[] args){
-        AudioThread main = new AudioThread(9001); //9001 is audio port
+        AudioThread main = new AudioThread(9001); //9001 is audio port by default
         main.start();
     }
 
-    public ChatServerMain(Socket socket) {
-        InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
+    public AudioThread(int port) {
+        try{
+            serverSocket = new ServerSocket(port);
+        } catch (Exception e){ e.printStackTrace(); }
     }
-    
+
     public void run(){
-        while (isRunning) {
-            try {
-                System.out.println("New audio connection from "+clientSocket.getInetAddress().toString());
-            } catch (IOException e) {
-                System.out.println("Accept failed: " + e);
-                System.out.println("Server has stopped running.");
-                System.exit(1);
+        isRunning = true;
+        try {
+            //connect clients...
+            clientSocket1 = serverSocket.accept();
+            System.out.println("New audio connection from "+clientSocket1.getInetAddress().toString());
+            clientSocket2 = serverSocket.accept();
+            System.out.println("New audio connection from "+clientSocket2.getInetAddress().toString());
+
+            //set up I/O
+            in1 = clientSocket1.getInputStream(); in2 = clientSocket2.getInputStream();
+            out1 = clientSocket1.getOutputStream(); out2 = clientSocket2.getOutputStream();
+            client1Data = new byte[128000];
+            client2Data = new byte[128000];
+
+            int client1BytesRead;
+            int client2BytesRead;
+            //then facilitate data transfer
+            while (isRunning){
+                client1BytesRead = in1.read(client1Data, 0, client1Data.length);
+                client2BytesRead = in2.read(client2Data, 0, client2Data.length);
+                if (client2BytesRead >= 0){ //send data from client 2 to client 1
+                    out1.write(client2Data);
+                }
+                if (client1BytesRead >= 0){ //send data from client 1 to client 2
+                    out2.write(client1Data);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public void stopRunning(){
+        try{
+        isRunning = false; clientSocket1.close(); clientSocket2.close();
+        } catch (Exception e){ e.printStackTrace(); }
     }
 }
