d5eddf87df3e1eff4923a2c256bda61b2c476610
Alec Zadikian <alec@genmobapp.com>
2013-09-22 21:41:42 -0700

    packages

---
+++ project/Garbage/MenuThing.java
@@ -0,0 +1,16 @@
+package Garbage;
+
+import java.awt.*;
+import java.io.InputStream;
+import java.util.*;
+import javax.swing.*;
+/**
+ * Write a description of class MenuThing here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MenuThing extends JFrame
+{
+    
+}
